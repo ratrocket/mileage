e0a827a9945ebafec0891c442ec0b35dedf0a793
Stuart Fletcher <stuartf@subpop.com>
2011-06-08 15:26:59 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mileage::Application.config.secret_token = '1360cb93b3ff8f39b58ac23996b9031a662a02eb45cda436a70b68965ce870c458ae2d637cd91e2af3f3c6da259145ea3d1dfddb64fe957bc838fcc86b6235a0'
