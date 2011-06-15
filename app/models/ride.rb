6de7f34c73b3a8a4cf19fc0325c2568ee0252036
Stuart Fletcher <stuartf@subpop.com>
2011-06-15 15:44:09 -0700

    cleaning up code; date stuff

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -4,28 +4,45 @@ class Ride < ActiveRecord::Base
 
   # my week goes from Sunday to Saturday...
   scope :weekly, lambda {|*args|
-    d = args.first || Date.today
-    {:conditions => {:date => d.monday.yesterday..d.sunday.yesterday}}
+    d = self.str2date(args.first) || Date.today
+    where(:date => d.monday.yesterday..d.sunday.yesterday)
   }
   scope :monthly, lambda {|*args|
-    d = args.first || Date.today
-    {:conditions => {:date => d.beginning_of_month..d.end_of_month}}
+    d = self.str2date(args.first) || Date.today
+    where(:date => d.beginning_of_month..d.end_of_month)
   }
   scope :yearly, lambda {|*args|
-    d = args.first || Date.today
-    {:conditions => {:date => d.beginning_of_year..d.end_of_year}}
+    d = self.str2date(args.first) || Date.today
+    where(:date => d.beginning_of_year..d.end_of_year)
   }
 
   before_save :convert_units
 
-  # TODO poorly named?  The following class methods are all "summary"
-  # methods in nature, after all.
+  # TODO poorly named?
   def summary
     [date.to_s, "#{miles} miles", "#{kilometers} km", bike.name, description].
       join("\n")
   end
 
   private
+
+  # TODO:   this shouldn't be in Ride
+  # TODO 2: if year is omitted, assume current year
+  # TODO 3: there's not much error handling here
+  def self.str2date(str)
+    return str if str.kind_of? Date
+    if str =~ /\//
+      sep = '/'
+    elsif str =~ /-/
+      sep = '-'
+    else
+      return nil  # punt
+    end
+
+    m, d, y = str.split(sep).map(&:to_i)
+    y = y + 2000 if y < 100
+    date = Date.new(y, m, d)
+  end
 
   # TODO have to make sure only one or the other is present (in create()
   # or new())
