b6d41aa9b4bed23d0301c07f8fd05ddd597f8ffa
Stuart Fletcher <stuartf@subpop.com>
2011-10-04 11:40:10 -0700

    Added slugifying for bike names in before_save.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,21 +1,4 @@
 module ApplicationHelper
-  def to_ascii(s)
-    require 'unicode'
-    # http://www.jroller.com/obie/entry/fix_that_tranny_add_to
-    Unicode.normalize_KD(s).unpack('U*').select {|cp| cp < 127}.pack('U*')
-  end
-
-  def slugify(str)
-    ret = to_ascii(str).downcase
-    ret.gsub!(/(\s\&\s)|(\s\&amp\;\s)/, ' and ') # convert & to "and"
-    ret.gsub!(/\'/, '')   #replace non-chars
-    ret.gsub!(/\W/, ' ')  #replace non-chars
-    ret.gsub!(/\ +/, '_') #replace spaces
-    ret.gsub!(/(_)$/, '') #trailing underscores
-    ret.gsub!(/^(_)/, '') #leading underscores
-    ret
-  end
-
   def nice_number(num)
     number_with_precision(num, :precision => 2,
       :strip_insignificant_zeros => true)
--- app/models/bike.rb
+++ app/models/bike.rb
@@ -2,4 +2,12 @@ class Bike < ActiveRecord::Base
   has_many :rides
   has_many :notes
   belongs_to :user
+
+  before_save :slugify
+
+  private
+
+  def slugify
+    self.slug = Util.slugify self.name
+  end
 end
