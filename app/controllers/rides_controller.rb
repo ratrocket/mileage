079dfeb5faed85903ec689532d5cd632cbc5aeab
Stuart Fletcher <stuartf@subpop.com>
2011-11-13 15:33:13 -0800

    Added number of days ridden/mo. and percentage of days.

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -14,6 +14,14 @@ class RidesController < ApplicationController
         Array.new(3)  # [nil, nil, nil]
       end
 
+    # TODO: setting a default year is taken care of in the scope in the
+    # model, but maybe it's better to do it here and make the model
+    # dumber?  NMW, for now I have to set a default year here TOO, which
+    # isn't great...
+    if !month.nil? && month > 0 && (year.nil? || year.zero?)
+      year = Date.today.year
+    end
+
     @rides = @user.rides.reals.bike(bike).month_and_year(month, year).sort_by(&:date)
     @services = @user.rides.reals.service.bike(bike).month_and_year(month, year).sort_by(&:date)
     @rides_only = @rides - @services
@@ -21,6 +29,19 @@ class RidesController < ApplicationController
     # these could perhaps be simpler?
     @rides_by_bike = @rides_only.map(&:bike).uniq.map {|b| [b.name, @rides_only.select {|r| r.bike_id == b.id}.size, @rides_only.select {|r| r.bike_id == b.id}.map(&:miles).sum]}
     @services_by_bike = @services.map(&:bike).uniq.map {|b| [b.name, @services.select {|r| r.bike_id == b.id}.size]}
+
+    # count # of days/month that were ridden
+    @days_ridden, @total_days =
+      if year && month
+        days = if (year == Date.today.year && month == Date.today.month)
+                 Date.today.day
+               else
+                 Date.civil(year, month, -1).day
+               end
+        [@rides_only.map(&:date).uniq.size, days]
+      else
+        [nil, nil]
+      end
   end
 
   def show
